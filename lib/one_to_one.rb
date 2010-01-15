6e8532b7cb57b519958c49f76ace1b51f2523ce2
David Kahn <david@nichecartel.com>
2010-01-14 21:44:53 -0800

    validates presence of parent

--- lib/one_to_one.rb
+++ lib/one_to_one.rb
@@ -1,9 +1,16 @@
 module OneToOne
-  module ClassMethods
-    alias_method(:original_belongs_to, :belongs_to)
-    def belongs_to(parent_name)
-      self.set_table_name(parent_name.to_s.pluralize)
-      self.original_belongs_to
+  def self.included(base)
+    base.class_eval do
+      class << self
+        alias_method(:original_belongs_to, :belongs_to)
+        private :original_belongs_to
+        
+        def belongs_to(parent_name)
+          set_table_name(parent_name.to_s.pluralize)
+          original_belongs_to(parent_name)
+        end
+      end
+      validates_presence_of :parent
     end
   end
 end
--- spec/one_to_one/one_to_one_spec.rb
+++ spec/one_to_one/one_to_one_spec.rb
@@ -10,7 +10,6 @@ describe OneToOne do
       Parent = Class.new(ActiveRecord::Base) do
       end
       Child = Class.new(ActiveRecord::Base) do
-        include OneToOne
       end
     end
     
@@ -22,21 +21,54 @@ describe OneToOne do
           end
           def set_table_name(table_name)
           end
+          def method_missing(method, *args, &block)
+            puts 'method skipped'
+          end
         end
       end
+      
       Foo.should_receive(:set_table_name).with('parents')
-      Foo.class_eval do
-        belongs_to :parent
+      
+      class Foo
+        include OneToOne       
+        belongs_to(:parent)
       end
+      
       Foo.instance_variable_get(:@original_called).should == true
     end
     it 'should use the same table as the Parent class' do
+      Child.class_eval do
+        include OneToOne
+        belongs_to(:parent)
+      end
       Child.table_name.should == Parent.table_name
     end
     it 'should not raise an error for the Child classes table not found' do
+      Child.class_eval do
+        include OneToOne
+        belongs_to(:parent)
+      end
       lambda do
         Child.new
       end.should_not raise_error(ActiveRecord::StatementInvalid, "Could not find table 'children'")
+    end
+    
+    it 'should raise an error if a Child instance is saved without an associated Parent instance' do
+      Child.class_eval do
+        include OneToOne
+        belongs_to(:parent)
+      end
+      child = Child.new
+      child.valid?
+      child.errors.should_not be_empty
+      child.errors.on('parent').should =~ /can't be blank/
+    end
+    it 'should use the class names in the error message' do
+      pending
+    end
+    
+    it 'should raise an error if another belongs_to association is declared' do
+      pending
     end
   end
   
