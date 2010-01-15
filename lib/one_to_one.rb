8a8801cb6dba22e03e94a3592d88c7ccee223c73
David Kahn <david@nichecartel.com>
2010-01-14 19:54:23 -0800

    3 passing tests for child table name, 1 new failing test for errors if saving child without parent

--- lib/one_to_one.rb
+++ lib/one_to_one.rb
@@ -1,3 +1,9 @@
 module OneToOne
-
+  module ClassMethods
+    alias_method(:original_belongs_to, :belongs_to)
+    def belongs_to(parent_name)
+      self.set_table_name(parent_name.to_s.pluralize)
+      self.original_belongs_to
+    end
+  end
 end
