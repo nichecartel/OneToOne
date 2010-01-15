bf327a90762892773ac0a5ab5f44c60c49b5c744
David Kahn <david@nichecartel.com>
2010-01-14 19:50:23 -0800

    3 failing tests for child table name

--- spec/one_to_one/one_to_one_spec.rb
+++ spec/one_to_one/one_to_one_spec.rb
@@ -4,14 +4,31 @@ describe OneToOne do
   context 'when included in the Child class' do
     before(:all) do
       CreateParents.migrate(:up)
-      at_exit { CreateParents.migrate(:down) }
+      at_exit { CreateParents.migrate(:down) }    
+    end
+    before(:each) do
       Parent = Class.new(ActiveRecord::Base) do
-        has_one :child
       end
       Child = Class.new(ActiveRecord::Base) do
         include OneToOne
+      end
+    end
+    
+    it 'should rename the expected child table when belongs_to is called' do
+      class Foo
+        class << self
+          def belongs_to(parent_class)
+            @original_called = true
+          end
+          def set_table_name(table_name)
+          end
+        end
+      end
+      Foo.should_receive(:set_table_name).with('parents')
+      Foo.class_eval do
         belongs_to :parent
       end
+      Foo.instance_variable_get(:@original_called).should == true
     end
     it 'should use the same table as the Parent class' do
       Child.table_name.should == Parent.table_name
